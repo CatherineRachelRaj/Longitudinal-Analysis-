74cb1416b82a36133aa85ecda5a7987baf8b459a
CatherineRachelRaj <123333419+CatherineRachelRaj@users.noreply.github.com>
2024-05-01 17:40:43 -0400

    Update Unconditional models.R

--- Unconditional models.R
+++ Unconditional models.R
@@ -3,8 +3,29 @@
 uc.mean=lmer(risk_behav ~ 1 + (1 |PID), data = l.filtered,REML = FALSE)
 summary(uc.mean)
 
+# AIC
+AIC(uc.mean)
+# BIC
+BIC(uc.mean)
+#  Deviance
+-2*logLik(uc.mean)
+#Likelihood
+logLik(uc.mean)
+# ICC Calculation
+icc(uc.mean)
+
 #unconditional growth model
 uc.growth=lmer(risk_behav~ time + (time | PID), data = l.filtered, REML = FALSE)
 summary(uc.growth)
 
+# AIC
+AIC(uc.growth)
+# BIC
+BIC(uc.growth)
+#  Deviance
+-2*logLik(uc.growth)
+#Likelihood
+logLik(uc.growth)
+# ICC Calculation
+icc(uc.growth)
 ```
