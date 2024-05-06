78b91246747290ab1fe558a5d0677630b3520a28
CatherineRachelRaj <123333419+CatherineRachelRaj@users.noreply.github.com>
2024-05-05 21:01:01 -0400

    OUTCOME: PEER DRUG USE

--- Real final.R
+++ Real final.R
@@ -4,11 +4,29 @@
 l.filtered$wave <- l.filtered$time - 1
 uc.growth=lmer(peer_drug_use ~ wave + (wave | PID), data = l.filtered, REML = FALSE)
 summary(uc.growth)
+# AIC
+AIC(uc.growth)
+# BIC
+BIC(uc.growth)
+#  Deviance
+-2*logLik(uc.growth)
+#Likelihood
+logLik(uc.growth)
+# ICC Calculation
 icc(uc.growth)# no warning about singularity 
 
 ### conditional growth model with main IV polysubstance use only
 c.iv1=lmer(peer_drug_use ~ discrimination*wave + (wave | PID), data = l.filtered, REML = FALSE)
 summary(c.iv1) 
+# AIC
+AIC(c.iv1)
+# BIC
+BIC(c.iv1)
+#  Deviance
+-2*logLik(c.iv1)
+#Likelihood
+logLik(c.iv1)
+# ICC Calculation
 icc(c.iv1)# no warning about singularity 
 
 ###conditional growth with main IV and additional time varying predictor
@@ -17,6 +35,15 @@ c.iv2=lmer(peer_drug_use ~ discrimination*wave
                             + race*discrimination 
                             + (wave | PID), data = l.filtered, REML = FALSE)
 summary(c.iv2) 
+# AIC
+AIC(c.iv2)
+# BIC
+BIC(c.iv2)
+#  Deviance
+-2*logLik(c.iv2)
+#Likelihood
+logLik(c.iv2)
+# ICC Calculation
 icc(c.iv2)# no warning about singularity 
 
 ###conditional growth with main IV and additional time varying predictor
@@ -28,6 +55,15 @@ c.iv3=lmer(peer_drug_use ~ discrimination*wave
                            + discrimination*education_head
                            + (wave | PID), data = l.filtered, REML = FALSE)
 summary(c.iv3) 
+# AIC
+AIC(c.iv3)
+# BIC
+BIC(c.iv3)
+#  Deviance
+-2*logLik(c.iv3)
+#Likelihood
+logLik(c.iv3)
+# ICC Calculation
 icc(c.iv3)# no warning about singularity 
 
 ```
