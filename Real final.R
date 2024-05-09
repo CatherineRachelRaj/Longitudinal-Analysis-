d71576d4dbe69b209f3e7619a65397eacdea9f90
CatherineRachelRaj <123333419+CatherineRachelRaj@users.noreply.github.com>
2024-05-08 23:35:12 -0400

    Updated 11:35

--- Real final.R
+++ Real final.R
@@ -38,8 +38,8 @@ logLik(uc.growth)
 icc(uc.growth)
 
 
-### conditional growth model with main IV discrimination only
-c.iv1=lmerTest::lmer(peer_drug_use~ discrimination*age+ (age | PID), 
+### conditional growth model with main IV education_head only
+c.iv1=lmerTest::lmer(peer_drug_use~ education_head*age+ (age | PID), 
             data=l.filtered, 
             REML=FALSE)
 
@@ -56,8 +56,8 @@ logLik(c.iv1)
 icc(c.iv1)
 
 ###conditional growth with main IV and all additional time-invariant predictors
-c.iv2=lmerTest::lmer(peer_drug_use~ discrimination*age +
-                       race + education_head +
+c.iv2=lmerTest::lmer(peer_drug_use~ education_head*age +
+                       race + discrimination +
                       (age|PID), 
             data=l.filtered, 
             REML = FALSE)
@@ -95,19 +95,18 @@ p1 + theme(
 
 #plot for conditional model with main IV only
 fixedc.iv1=fixef(c.iv1)
-dis1=fixedc.iv1[[1]]+fixedc.iv1[[7]]*age.levels
-dis2=fixedc.iv1[[1]]+fixedc.iv1[[7]]*age.levels+fixedc.iv1[[2]]+fixedc.iv1[[8]]*age.levels
-dis3=fixedc.iv1[[1]]+fixedc.iv1[[7]]*age.levels+fixedc.iv1[[3]]+fixedc.iv1[[9]]*age.levels
-dis4=fixedc.iv1[[1]]+fixedc.iv1[[7]]*age.levels+fixedc.iv1[[4]]+fixedc.iv1[[10]]*age.levels
-dis5=fixedc.iv1[[1]]+fixedc.iv1[[7]]*age.levels+fixedc.iv1[[5]]+fixedc.iv1[[11]]*age.levels
-dis6=fixedc.iv1[[1]]+fixedc.iv1[[7]]*age.levels+fixedc.iv1[[6]]+fixedc.iv1[[12]]*age.levels
+dis1=fixedc.iv1[[1]]+fixedc.iv1[[5]]*age.levels
+dis2=fixedc.iv1[[1]]+fixedc.iv1[[5]]*age.levels+fixedc.iv1[[2]]+fixedc.iv1[[6]]*age.levels
+dis3=fixedc.iv1[[1]]+fixedc.iv1[[5]]*age.levels+fixedc.iv1[[3]]+fixedc.iv1[[7]]*age.levels
+dis4=fixedc.iv1[[1]]+fixedc.iv1[[5]]*age.levels+fixedc.iv1[[4]]+fixedc.iv1[[8]]*age.levels
 
-civ1.model=data.frame(Age=rep(age.levels, times=6), Social_ties=c(dis1,dis2,dis3,dis4,dis5,dis6), Discrimination=rep(1:6,each=5))
-civ1.model$Discrimination= as.factor(civ1.model$Discrimination)
+ed_labels=c("<=High School", "Some College", "4 year College Degree", "Any Post Graduate")
+civ1.model=data.frame(Age=rep(age.levels, times=4), Social_ties=c(dis1,dis2,dis3,dis4), Education=rep(ed_labels,each=5))
+civ1.model$Education= as.factor(civ1.model$Education)
 
-p2=ggplot(data=civ1.model, aes(x=Age, y=Social_ties, col=Discrimination))+
+p2=ggplot(data=civ1.model, aes(x=Age, y=Social_ties, col=Education))+
    geom_line() + 
-   ggtitle("Growth Plot of High-Risk for \nEach Score of Discrimination")+
+   ggtitle("Growth Plot of High-Risk Social Ties for \nEach level of Education of Household Head")+
    theme_classic()+
    labs(x="Age",y="High-Risk Social Ties")
 p2 + theme(
@@ -116,64 +115,57 @@ p2 + theme(
   axis.title.y = element_text(size = 12, face="bold")   # Increase font size for y-axis label
 )
 
+
 ###plot for conditional model with main IV and additional time-invariant predictors
 
-###plot for conditional model with main IV, stratified by race controlling for education_head
 fixedef.iv2=fixef(c.iv2)
-#for race = Hispanic controlling for education_head
-r0dis1=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels
-r0dis2=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[2]]+fixedef.iv2[[15]]*age.levels
-r0dis3=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[3]]+fixedef.iv2[[16]]*age.levels
-r0dis4=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[4]]+fixedef.iv2[[17]]*age.levels
-r0dis5=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[5]]+fixedef.iv2[[18]]*age.levels
-r0dis6=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[6]]+fixedef.iv2[[19]]*age.levels
+#for race = Hispanic controlling for discrimination
+r0dis1=fixedef.iv2[[1]]+fixedef.iv2[[5]]*age.levels #race0 and ed=0
+r0dis2=fixedef.iv2[[1]]+fixedef.iv2[[5]]*age.levels+fixedef.iv2[[2]]+fixedef.iv2[[15]]*age.levels #race=0 and ed=1
+r0dis3=fixedef.iv2[[1]]+fixedef.iv2[[5]]*age.levels+fixedef.iv2[[3]]+fixedef.iv2[[16]]*age.levels
+r0dis4=fixedef.iv2[[1]]+fixedef.iv2[[5]]*age.levels+fixedef.iv2[[4]]+fixedef.iv2[[17]]*age.levels
 
 #for race=Non-Hispanic White
-r1dis1=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[8]]
-r1dis2=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[2]]+fixedef.iv2[[15]]*age.levels+fixedef.iv2[[8]]
-r1dis3=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[3]]+fixedef.iv2[[16]]*age.levels+fixedef.iv2[[8]]
-r1dis4=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[4]]+fixedef.iv2[[17]]*age.levels+fixedef.iv2[[8]]
-r1dis5=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[5]]+fixedef.iv2[[18]]*age.levels+fixedef.iv2[[8]]
-r1dis6=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[6]]+fixedef.iv2[[19]]*age.levels+fixedef.iv2[[8]]
+r1dis1=fixedef.iv2[[1]]+fixedef.iv2[[5]]*age.levels+fixedef.iv2[[6]] #r-1 and ed=0
+r1dis2=fixedef.iv2[[1]]+fixedef.iv2[[5]]*age.levels+fixedef.iv2[[2]]+fixedef.iv2[[15]]*age.levels+fixedef.iv2[[6]] #r=1 and ed=1
+r1dis3=fixedef.iv2[[1]]+fixedef.iv2[[5]]*age.levels+fixedef.iv2[[3]]+fixedef.iv2[[16]]*age.levels+fixedef.iv2[[6]] #r=1 and ed=2
+r1dis4=fixedef.iv2[[1]]+fixedef.iv2[[5]]*age.levels+fixedef.iv2[[4]]+fixedef.iv2[[17]]*age.levels+fixedef.iv2[[6]]
 
 #for race=Non-Hispanicn Black 
-r2dis1=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[9]]
-r2dis2=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[2]]+fixedef.iv2[[15]]*age.levels+fixedef.iv2[[9]]
-r2dis3=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[3]]+fixedef.iv2[[16]]*age.levels+fixedef.iv2[[9]]
-r2dis4=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[4]]+fixedef.iv2[[17]]*age.levels+fixedef.iv2[[9]]
-r2dis5=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[5]]+fixedef.iv2[[18]]*age.levels+fixedef.iv2[[9]]
-r2dis6=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[6]]+fixedef.iv2[[19]]*age.levels+fixedef.iv2[[9]]
+r2dis1=fixedef.iv2[[1]]+fixedef.iv2[[5]]*age.levels+fixedef.iv2[[7]]
+r2dis2=fixedef.iv2[[1]]+fixedef.iv2[[5]]*age.levels+fixedef.iv2[[2]]+fixedef.iv2[[15]]*age.levels+fixedef.iv2[[7]]
+r2dis3=fixedef.iv2[[1]]+fixedef.iv2[[5]]*age.levels+fixedef.iv2[[3]]+fixedef.iv2[[16]]*age.levels+fixedef.iv2[[7]]
+r2dis4=fixedef.iv2[[1]]+fixedef.iv2[[5]]*age.levels+fixedef.iv2[[4]]+fixedef.iv2[[17]]*age.levels+fixedef.iv2[[7]]
+
 
 #for race=Non-Hispanic Asian
-r3dis1=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[10]]
-r3dis2=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[2]]+fixedef.iv2[[15]]*age.levels+fixedef.iv2[[10]]
-r3dis3=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[3]]+fixedef.iv2[[16]]*age.levels+fixedef.iv2[[10]]
-r3dis4=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[4]]+fixedef.iv2[[17]]*age.levels+fixedef.iv2[[10]]
-r3dis5=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[5]]+fixedef.iv2[[18]]*age.levels+fixedef.iv2[[10]]
-r3dis6=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[6]]+fixedef.iv2[[19]]*age.levels+fixedef.iv2[[10]]
+r3dis1=fixedef.iv2[[1]]+fixedef.iv2[[5]]*age.levels+fixedef.iv2[[8]]
+r3dis2=fixedef.iv2[[1]]+fixedef.iv2[[5]]*age.levels+fixedef.iv2[[2]]+fixedef.iv2[[15]]*age.levels+fixedef.iv2[[8]]
+r3dis3=fixedef.iv2[[1]]+fixedef.iv2[[5]]*age.levels+fixedef.iv2[[3]]+fixedef.iv2[[16]]*age.levels+fixedef.iv2[[8]]
+r3dis4=fixedef.iv2[[1]]+fixedef.iv2[[5]]*age.levels+fixedef.iv2[[4]]+fixedef.iv2[[17]]*age.levels+fixedef.iv2[[8]]
+
 
 #for race=Non-Hispanic Other
-r4dis1=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[12]]
-r4dis2=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[2]]+fixedef.iv2[[15]]*age.levels+fixedef.iv2[[11]]
-r4dis3=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[3]]+fixedef.iv2[[16]]*age.levels+fixedef.iv2[[11]]
-r4dis4=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[4]]+fixedef.iv2[[17]]*age.levels+fixedef.iv2[[11]]
-r4dis5=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[5]]+fixedef.iv2[[18]]*age.levels+fixedef.iv2[[11]]
-r4dis6=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[6]]+fixedef.iv2[[19]]*age.levels+fixedef.iv2[[11]]
+r4dis1=fixedef.iv2[[1]]+fixedef.iv2[[5]]*age.levels+fixedef.iv2[[9]]
+r4dis2=fixedef.iv2[[1]]+fixedef.iv2[[5]]*age.levels+fixedef.iv2[[2]]+fixedef.iv2[[15]]*age.levels+fixedef.iv2[[9]]
+r4dis3=fixedef.iv2[[1]]+fixedef.iv2[[5]]*age.levels+fixedef.iv2[[3]]+fixedef.iv2[[16]]*age.levels+fixedef.iv2[[9]]
+r4dis4=fixedef.iv2[[1]]+fixedef.iv2[[5]]*age.levels+fixedef.iv2[[4]]+fixedef.iv2[[17]]*age.levels+fixedef.iv2[[9]]
 
 race_labels=c("Hispanic", "Non-Hispanic White","Non-Hispanic Black", "Non-Hispanic Asian", "Non-Hispanic Other")
-race_model=data.frame(Age=rep(age.levels, times=30),
-                      Social_ties=c(r0dis1,r0dis2,r0dis3,r0dis4,r0dis5, r0dis6,
-                                    r1dis1,r1dis2,r1dis3,r1dis4,r1dis5, r1dis6,
-                                    r2dis1,r2dis2,r2dis3,r2dis4,r2dis5, r2dis6,
-                                    r3dis1,r3dis2,r3dis3,r3dis4,r3dis5, r3dis6,
-                                    r4dis1,r4dis2,r4dis3,r4dis4,r4dis5, r4dis6),
-                      Discrimination=rep(1:6, each=5),
-                      Race=rep(race_labels,each=30))
-race_model$Discrimination= as.factor(race_model$Discrimination)
+race_model=data.frame(Age=rep(age.levels, times=20),
+                      Social_ties=c(r0dis1,r0dis2,r0dis3,r0dis4,
+                                    r1dis1,r1dis2,r1dis3,r1dis4,
+                                    r2dis1,r2dis2,r2dis3,r2dis4,
+                                    r3dis1,r3dis2,r3dis3,r3dis4,
+                                    r4dis1,r4dis2,r4dis3,r4dis4),
+                      Education=rep(ed_labels, each=5),
+                      Race=rep(race_labels,each=20))
+race_model$Education= as.factor(race_model$Education)
+
 
 p3=ggplot(data = race_model) +
-  geom_line(aes(x = Age, y = Social_ties, col = Discrimination)) +
-  facet_wrap(~ Race)+ggtitle("Growth plot of High-Risk Social Ties for each score of Discrimination \n for Different Levels of Race \n (controlling for Education of Household Head)")+
+  geom_line(aes(x = Age, y = Social_ties, col =Education)) +
+  facet_wrap(~ Race)+ggtitle("Growth plot of High-Risk Social Ties for \n level of Education of Household Head \n for Different Levels of Race \n (controlling for Perceived Discrimination score)")+
   theme_classic()+
   labs(y="High-Risk Social Ties")
   
@@ -183,53 +175,65 @@ p3 + theme(
   axis.title.y = element_text(size = 12, face="bold")   # Increase font size for y-axis label
 )
 
+#for discr=1 controlling for race
+ed0dis1=fixedef.iv2[[1]]+fixedef.iv2[[5]]*age.levels
+ed0dis2=fixedef.iv2[[1]]+fixedef.iv2[[5]]*age.levels+fixedef.iv2[[2]]+fixedef.iv2[[15]]*age.levels
+ed0dis3=fixedef.iv2[[1]]+fixedef.iv2[[5]]*age.levels+fixedef.iv2[[3]]+fixedef.iv2[[16]]*age.levels
+ed0dis4=fixedef.iv2[[1]]+fixedef.iv2[[5]]*age.levels+fixedef.iv2[[4]]+fixedef.iv2[[17]]*age.levels
 
-#for education_head= <=High School/GED controlling for race
-ed0dis1=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels
-ed0dis2=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[2]]+fixedef.iv2[[15]]*age.levels
-ed0dis3=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[3]]+fixedef.iv2[[16]]*age.levels
-ed0dis4=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[4]]+fixedef.iv2[[17]]*age.levels
-ed0dis5=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[5]]+fixedef.iv2[[18]]*age.levels
-ed0dis6=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[6]]+fixedef.iv2[[19]]*age.levels
 
-#for education_head= Some College controlling for race
-ed1dis1=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels++fixedef.iv2[[12]]
-ed1dis2=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[2]]+fixedef.iv2[[15]]*age.levels+fixedef.iv2[[12]]
-ed1dis3=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[3]]+fixedef.iv2[[16]]*age.levels+fixedef.iv2[[12]]
-ed1dis4=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[4]]+fixedef.iv2[[17]]*age.levels+fixedef.iv2[[12]]
-ed1dis5=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[5]]+fixedef.iv2[[18]]*age.levels+fixedef.iv2[[12]]
-ed1dis6=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[6]]+fixedef.iv2[[19]]*age.levels+fixedef.iv2[[12]]
+#for discr=2 controlling for race
+ed1dis1=fixedef.iv2[[1]]+fixedef.iv2[[5]]*age.levels++fixedef.iv2[[10]]
+ed1dis2=fixedef.iv2[[1]]+fixedef.iv2[[5]]*age.levels+fixedef.iv2[[2]]+fixedef.iv2[[15]]*age.levels+fixedef.iv2[[10]]
+ed1dis3=fixedef.iv2[[1]]+fixedef.iv2[[5]]*age.levels+fixedef.iv2[[3]]+fixedef.iv2[[16]]*age.levels+fixedef.iv2[[10]]
+ed1dis4=fixedef.iv2[[1]]+fixedef.iv2[[5]]*age.levels+fixedef.iv2[[4]]+fixedef.iv2[[17]]*age.levels+fixedef.iv2[[10]]
 
-#for education_head= 4 -year College controlling for race
-ed2dis1=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels++fixedef.iv2[[13]]
-ed2dis2=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[2]]+fixedef.iv2[[15]]*age.levels+fixedef.iv2[[13]]
-ed2dis3=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[3]]+fixedef.iv2[[16]]*age.levels+fixedef.iv2[[13]]
-ed2dis4=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[4]]+fixedef.iv2[[17]]*age.levels+fixedef.iv2[[13]]
-ed2dis5=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[5]]+fixedef.iv2[[18]]*age.levels+fixedef.iv2[[13]]
-ed2dis6=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[6]]+fixedef.iv2[[19]]*age.levels+fixedef.iv2[[13]]
+#for discr=3 controlling for race
+ed2dis1=fixedef.iv2[[1]]+fixedef.iv2[[5]]*age.levels++fixedef.iv2[[11]]
+ed2dis2=fixedef.iv2[[1]]+fixedef.iv2[[5]]*age.levels+fixedef.iv2[[2]]+fixedef.iv2[[15]]*age.levels+fixedef.iv2[[11]]
+ed2dis3=fixedef.iv2[[1]]+fixedef.iv2[[5]]*age.levels+fixedef.iv2[[3]]+fixedef.iv2[[16]]*age.levels+fixedef.iv2[[11]]
+ed2dis4=fixedef.iv2[[1]]+fixedef.iv2[[5]]*age.levels+fixedef.iv2[[4]]+fixedef.iv2[[17]]*age.levels+fixedef.iv2[[11]]
 
-#for education_head= Any Post-Graduate work controlling for race
-ed3dis1=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels++fixedef.iv2[[14]]
-ed3dis2=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[2]]+fixedef.iv2[[15]]*age.levels+fixedef.iv2[[14]]
-ed3dis3=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[3]]+fixedef.iv2[[16]]*age.levels+fixedef.iv2[[14]]
-ed3dis4=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[4]]+fixedef.iv2[[17]]*age.levels+fixedef.iv2[[14]]
-ed3dis5=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[5]]+fixedef.iv2[[18]]*age.levels+fixedef.iv2[[14]]
-ed3dis6=fixedef.iv2[[1]]+fixedef.iv2[[7]]*age.levels+fixedef.iv2[[6]]+fixedef.iv2[[19]]*age.levels+fixedef.iv2[[14]]
+
+#for discr=4 controlling for race
+ed3dis1=fixedef.iv2[[1]]+fixedef.iv2[[5]]*age.levels++fixedef.iv2[[12]]
+ed3dis2=fixedef.iv2[[1]]+fixedef.iv2[[5]]*age.levels+fixedef.iv2[[2]]+fixedef.iv2[[15]]*age.levels+fixedef.iv2[[12]]
+ed3dis3=fixedef.iv2[[1]]+fixedef.iv2[[5]]*age.levels+fixedef.iv2[[3]]+fixedef.iv2[[16]]*age.levels+fixedef.iv2[[12]]
+ed3dis4=fixedef.iv2[[1]]+fixedef.iv2[[5]]*age.levels+fixedef.iv2[[4]]+fixedef.iv2[[17]]*age.levels+fixedef.iv2[[12]]
+
+#for discr=5 controlling for race
+ed4dis1=fixedef.iv2[[1]]+fixedef.iv2[[5]]*age.levels++fixedef.iv2[[13]]
+ed4dis2=fixedef.iv2[[1]]+fixedef.iv2[[5]]*age.levels+fixedef.iv2[[2]]+fixedef.iv2[[15]]*age.levels+fixedef.iv2[[13]]
+ed4dis3=fixedef.iv2[[1]]+fixedef.iv2[[5]]*age.levels+fixedef.iv2[[3]]+fixedef.iv2[[16]]*age.levels+fixedef.iv2[[13]]
+ed4dis4=fixedef.iv2[[1]]+fixedef.iv2[[5]]*age.levels+fixedef.iv2[[4]]+fixedef.iv2[[17]]*age.levels+fixedef.iv2[[13]]
+
+#for discr=6 controlling for race
+ed5dis1=fixedef.iv2[[1]]+fixedef.iv2[[5]]*age.levels++fixedef.iv2[[14]]
+ed5dis2=fixedef.iv2[[1]]+fixedef.iv2[[5]]*age.levels+fixedef.iv2[[2]]+fixedef.iv2[[15]]*age.levels+fixedef.iv2[[14]]
+ed5dis3=fixedef.iv2[[1]]+fixedef.iv2[[5]]*age.levels+fixedef.iv2[[3]]+fixedef.iv2[[16]]*age.levels+fixedef.iv2[[14]]
+ed5dis4=fixedef.iv2[[1]]+fixedef.iv2[[5]]*age.levels+fixedef.iv2[[4]]+fixedef.iv2[[17]]*age.levels+fixedef.iv2[[14]]
+
+
 
 ed_labels=c("<=High School", "Some College", "4 year College Degree", "Any Post Graduate")
 
 ed_model= data.frame(Age=rep(age.levels, times=24),
-                     Social_ties=c(ed0dis1,ed0dis2,ed0dis3,ed0dis4,ed0dis5,ed0dis6,
-                                   ed1dis1,ed1dis2,ed1dis3,ed1dis4,ed1dis5,ed1dis6,
-                                   ed2dis1,ed2dis2,ed2dis3,ed2dis4,ed2dis5,ed2dis6,
-                                   ed3dis1,ed3dis2,ed3dis3,ed3dis4,ed3dis5,ed3dis6),
-                     Discrimination=rep(1:6, each=5),
-                     Education=rep(ed_labels, each=30))
-ed_model$Discrimination=as.factor(ed_model$Discrimination)
+                     Social_ties=c(ed0dis1,ed0dis2,ed0dis3,ed0dis4,
+                                   ed1dis1,ed1dis2,ed1dis3,ed1dis4,
+                                   ed2dis1,ed2dis2,ed2dis3,ed2dis4,
+                                   ed3dis1,ed3dis2,ed3dis3,ed3dis4,
+                                   ed4dis1,ed4dis2,ed4dis3,ed4dis4,
+                                   ed5dis1,ed5dis2,ed5dis3,ed5dis4),
+                     Education=rep(ed_labels, each=5), 
+                     Discrimination=rep(1:6, each=20)
+              )
+ed_model$Education= as.factor(ed_model$Education)
+ed_model$Discrimination= as.factor(ed_model$Discrimination)
+
 
 p4=ggplot(data = ed_model) +
-  geom_line(aes(x = Age, y = Social_ties, col = Discrimination)) +
-  facet_wrap(~Education)+ggtitle("Growth plot of High-Risk Social Ties for each score of Discrimination \n for Different Levels of Education of Household Head \n (controlling for Race)")+
+  geom_line(aes(x = Age, y = Social_ties, col = Education)) +
+  facet_wrap(~Discrimination)+ggtitle("Growth plot of High-Risk Social Ties for \n each level of Education of Household Head \n for each score of Discrimination \n (controlling for Race)")+
   theme_classic()+
   labs(y="High-Risk Social Ties")
   
